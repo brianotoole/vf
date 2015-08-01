46ddb3b95205004b27902ea455b3fbfac5e5c7c6
botoole <botoole518@gmail.com>
2015-08-01 17:00:08 -0400

    archives

--- wp-content/themes/vf/archive.php
+++ wp-content/themes/vf/archive.php
@@ -8,19 +8,72 @@ get_header(); ?>
 
 	<header class="entry-header">
 	</header><!-- .entry-header -->
-
+     
 	<div class="grid">
 		<div id="primary" class="col-9-12">
-			<main id="main" class="site-main" role="main">
+		  <main id="main" class="site-main" role="main">
+			<div class="entry-content">
+			<h2 class="page-title animate fadeIn">
+				<?php
+						if ( is_category() ) :
+							printf( __( ' Category %s', 'vf' ), '<span>' . single_cat_title() . '</span>' );
 
-			<?php while ( have_posts() ) : the_post(); ?>
+						elseif ( is_tag() ) :
+							single_tag_title();
+
+						elseif ( is_author() ) :
+							printf( __( 'Archives: %s', 'vf' ), '<span class="vcard">' . get_the_author() . '</span>' );
+
+						elseif ( is_day() ) :
+							printf( __( 'Archives: %s', 'vf' ), '<span>' . get_the_date() . '</span>' );
+
+						elseif ( is_month() ) :
+							printf( __( 'Archives: %s', 'vf' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'vf' ) ) . '</span>' );
+
+						elseif ( is_year() ) :
+							printf( __( 'Archives: %s', 'vf' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'vf' ) ) . '</span>' );
+
+						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+							_e( 'Asides', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+							_e( 'Galleries', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+							_e( 'Images', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+							_e( 'Videos', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+							_e( 'Quotes', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+							_e( 'Links', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+							_e( 'Statuses', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+							_e( 'Audios', 'vf' );
+
+						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+							_e( 'Chats', 'vf' );
+
+						else :
+							_e( 'Archives', 'vf' );
+
+						endif;
+					?>
+			</h2>
 			
+			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', '' ); ?>
 
 			<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
+		    </div><!--/.entry-content-->
+		  </main><!-- #main -->
 		</div><!-- #primary -->
 
 	<?php get_sidebar(); ?>
--- wp-content/themes/vf/content.php
+++ wp-content/themes/vf/content.php
@@ -3,18 +3,13 @@
  * The template used for displaying news/blog content
  */
 ?>
-<?php $query = new WP_Query( 'posts_per_page=-1' ); //show all ?>
-<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 
 
 <div class="col-sm-12 descrip">
-<h2 class="page-title animate fadeIn"><?php the_title(); ?></h2>
-<p class="date"><?php echo get_the_date( 'l / F d / Y' ); ?></p>  
-<p><?php the_excerpt() ?></p>
+	<h3 class="news-title animate fadeIn"><?php the_title(); ?></h3>
+	<p class="date">
+	  <?php echo get_the_date( 'l / F d / Y' ); ?>
+	  <?php the_excerpt() ?>
+	</p>  
 </div>
 
-<?php endwhile; 
- wp_reset_postdata();
- else : ?>
-<p><?php _e( 'There are no news articles at this time.' ); ?></p>
-<?php endif; ?>
