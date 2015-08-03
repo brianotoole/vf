594128045a5e02e7737aa7c264941b715903e4d2
brianotoole <botoole518@gmail.com>
2015-08-03 15:28:06 -0400

    home custom fields

--- wp-content/themes/vf/content.php
+++ wp-content/themes/vf/content.php
@@ -6,7 +6,9 @@
 
 
 <div class="col-sm-12 descrip">
+  <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">  
 	<h3 class="news-title animate fadeIn"><?php the_title(); ?></h3>
+  </a>
 	<p class="date">
 	  <?php echo get_the_date( 'l / F d / Y' ); ?>
 	  <?php the_excerpt() ?>
--- wp-content/themes/vf/page-home.php
+++ wp-content/themes/vf/page-home.php
@@ -27,17 +27,17 @@ get_header(); ?>
 		<div class="col-sm-4">
 			<img src="http://placeimg.com/150/150/any">
 			<h3>Americorps</h3>
-			<p>Americorps members dedicate a year of their life to service in Florida. We make it happen.</p>
+			<p><?php the_field('home_wwd_left'); ?></p>
 		</div><hr class="visible-xs">
 		<div class="col-sm-4">
 			<img src="http://placeimg.com/150/150/any">
 			<h3>Florida<br />Volunteers</h3>
-			<p>Americorps members dedicate a year of their life to service in Florida. We make it happen.</p>
+			<p><?php the_field('home_wwd_middle'); ?></p>
 		</div><hr class="visible-xs">
 		<div class="col-sm-4">
 			<img src="http://placeimg.com/150/150/any">
 			<h3>Emergency<br />Management</h3>
-			<p>Americorps members dedicate a year of their life to service in Florida. We make it happen.</p>
+			<p><?php the_field('home_wwd_right'); ?></p>
 		</div>
 	</div><!--/.intro-->
 </section><!--/.what-we-do-->
@@ -57,25 +57,19 @@ get_header(); ?>
 			<img src="http://placeimg.com/475/500/any">
 		</div>
 		<div class="col-sm-8">
-			<p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat."</p>
+			<p>"<?php the_field('home_quote_text1'); ?>"</p>
 			<p>
-			  <strong>Quote Name</strong> / 
-			  <span>Quote Position</span> / 
-			  <span>Quote Company</span>
+			  <strong><?php the_field('home_quote_attr1'); ?></strong>
 			</p><hr>
 			
-			<p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat."</p>
+			<p>"<?php the_field('home_quote_text2'); ?>"</p>
 			<p>
-			  <strong>Quote Name</strong> / 
-			  <span>Quote Position</span> / 
-			  <span>Quote Company</span>
+			  <strong><?php the_field('home_quote_attr2'); ?></strong>
 			</p><hr>
 			
-			<p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat."</p>
+			<p>"<?php the_field('home_quote_text3'); ?>"</p>
 			<p>
-			  <strong>Quote Name</strong> / 
-			  <span>Quote Position</span> / 
-			  <span>Quote Company</span>
+			  <strong><?php the_field('home_quote_attr3'); ?></strong>
 			</p>
 		</div>
 	</div><!--/.intro-->
