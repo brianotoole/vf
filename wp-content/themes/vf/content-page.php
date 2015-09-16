a95f8f6eb2f9fbf45b97c04835a158b60d845313
brianotoole <botoole518@gmail.com>
2015-09-16 12:24:06 -0400

    breadcrumb styles

--- wp-content/themes/vf/content-page.php
+++ wp-content/themes/vf/content-page.php
@@ -6,12 +6,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content">
-		<div class="breadcrumb-nav">
-			<?php
+		<div class="nav-breadcrumb">
+			<?php //show child pages under current page
 			$children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
 			  if ($children) { ?>
 			  <ul>
-			  <?php echo $children; ?>
+			  	<?php echo $children; ?>
 			  </ul>
 			<?php } ?>
 		</div>
