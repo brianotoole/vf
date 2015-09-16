d59e07c163a32f6da34a0d3575794891e0a34127
brianotoole <botoole518@gmail.com>
2015-09-16 12:36:02 -0400

    bradcrumbs to conditionally display

--- wp-content/themes/vf/content-page.php
+++ wp-content/themes/vf/content-page.php
@@ -6,15 +6,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content">
-		<div class="nav-breadcrumb">
-			<?php //show child pages under current page
-			$children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
-			  if ($children) { ?>
-			  <ul>
-			  	<?php echo $children; ?>
-			  </ul>
-			<?php } ?>
-		</div>
+
+			<?php //if current page has children...
+			$children = get_pages('child_of='.$post->ID);
+			  if( count( $children ) != 0 ) { 
+			      $breadcrumbs = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
+				  echo '<div class="nav-breadcrumb"><ul>'.$breadcrumbs.'</ul></div>'; 
+			  }
+			  else { 
+				  //hide div
+			  }
+			?>
+			
 		<h2 class="page-title animate fadeIn"><?php the_title(); ?></h2>
 		
 		<?php the_content(); ?>
